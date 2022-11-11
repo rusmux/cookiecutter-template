3d9cf5bbfa5dc076f5821a5707bd165759793c25
Ruslan Mukhametshin <rusmux21@gmail.com>
2022-11-11 21:41:34 +0300

    Update template

--- {{ cookiecutter.repo_name }}/src/data/make_dataset.py
+++ {{ cookiecutter.repo_name }}/src/data/make_dataset.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+"""Script for creating processed data."""
+
 import logging
 from pathlib import Path
 
@@ -10,16 +11,16 @@ from dotenv import find_dotenv, load_dotenv
 @click.argument("input_filepath", type=click.Path(exists=True))
 @click.argument("output_filepath", type=click.Path())
 def main(input_filepath, output_filepath):
-    """Runs data processing scripts to turn raw data from (../raw) into
-    cleaned data ready to be analyzed (saved in ../processed).
+    """Preprocess raw data from (../raw) into cleaned data ready to be analyzed (saved in ../processed).
+
+    Args:
+        input_filepath (string): Path to the raw data.
+        output_filepath (string): Path to the processed data.
     """
-    logger = logging.getLogger(__name__)
-    logger.info("making final data set from raw data")
-
-
-if __name__ == "__main__":
     log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_format)
+    logger = logging.getLogger(__name__)
+    logger.info("making final data set from raw data")
 
     # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
@@ -28,4 +29,6 @@ if __name__ == "__main__":
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
 
+
+if __name__ == "__main__":
     main()
